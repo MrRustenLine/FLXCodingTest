3c011356a2461e710fa513418a1fd2b28a1ad667
Rusten Line <rusten.line@ga.gov.au>
2022-06-06 12:22:46 +0400

    added missing files to Github and cleaned code

---
+++ FlexeraCodingTest/IQuery.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexeraCodingTest
+{
+    public interface IQuery
+    {
+        public string DataFile {get; set; }
+
+        public string AppID { get; set; }
+    }
+}
---
+++ FlexeraCodingTest/Query.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlexeraCodingTest
+{
+    public class Query: IQuery
+    {
+        private string? dataFile;
+        private string? appID;
+
+        public string DataFile
+        {
+            get { return dataFile; }
+            set { dataFile = value; }
+        }
+
+        public string AppID
+        {
+            get { return appID; }
+            set { appID = value; }
+        }
+
+    }
+}
