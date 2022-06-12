484a9fcf457e88daef15549ab6e8fe30a76c8cf3
Rusten Line <rusten.line@gmail.com>
2022-06-12 15:02:39 +0400

    recreated repo after renaming it

---
+++ FLXCodingTest/Computer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FLXCodingTest
+{
+    internal struct Computer
+    {
+        internal int ComputerID { get; set; }
+        internal Computer(int computerID, int userID, int applicationID, string computerType )
+        {
+            ComputerID = computerID;
+            UserID = userID;
+            ApplicationID = applicationID;
+            ComputerType = computerType.ToLower();
+
+        }
+        internal int UserID { get; set; }
+        internal int ApplicationID { get; set; }
+
+        internal string ComputerType { get; set; }
+
+    }
+}
